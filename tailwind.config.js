f8b0bcb74518776eaf63069921aea5de221d4545
MusaddekAli <musaddekali.dev@gmail.com>
2023-09-27 09:11:43 +0600

    migration done and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '*.{html,js}',
-  ],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    // screens: {
+    //   sm: "576px",
+    //   md: "768px",
+    //   lg: "992px",
+    //   xl: "1200px",
+    //   xxl: "1400px",
+    // },
+    container: {
+      center: true,
+      padding: ".75rem",
+    },
+    extend: {
+      fontFamily: {
+        Creattion: ["Creattion"],
+      },
+      colors: {
+        primary: "#418ec6",
+        secondary: "#1e2762",
+        accent: "#7b2049",
+      },
+    },
   },
   plugins: [],
-}
-
+};
